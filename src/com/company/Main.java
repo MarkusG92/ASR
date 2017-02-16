be0f099530bca4a12ffc170a7da2ecdbefa0486f
Markus Gößmann <goessmannma53784@th-nuernberg.de>
2017-02-16 10:13:14 +0100

    Initial project checking

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World ");
+
+    }
+}
